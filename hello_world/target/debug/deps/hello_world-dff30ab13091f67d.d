c458afcf5045227a968a0565aae8f930c785f0d1
kaputte <kaputte@live.jp>
2017-08-06 23:14:55 +0900

    Finish introduction

---
+++ hello_world/target/debug/deps/hello_world-dff30ab13091f67d.d
@@ -0,0 +1,5 @@
+/home/brauschen/learn-rust/hello_world/target/debug/deps/hello_world-dff30ab13091f67d: src/main.rs
+
+/home/brauschen/learn-rust/hello_world/target/debug/deps/hello_world-dff30ab13091f67d.d: src/main.rs
+
+src/main.rs:
